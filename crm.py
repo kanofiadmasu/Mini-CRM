466b4fb25ad76a35e67806237ba188231c08d636
Kanofi <lukakanofi@gmail.com>
2025-09-08 22:57:56 -0400

    Added the project adding feature

--- crm.py
+++ crm.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 import re 
+import json
 
 class Clients(): 
     def __init__(self, name, phone_num, company, notes): 
@@ -19,6 +21,44 @@ class Clients():
             line =  f'{self.name} | {self.email} | {self.phone} | {self.company} | {self.notes}\n'
             client_file.write(line)
 
+'''Projects Class related to the project Sub Menu'''
+class Projects: #Project adding and creation
+    def __init__(self, client_name, project_type, status, deadline):
+        self.client_name = client_name
+        self.project = project_type
+        self.status = status
+        self.start_date = datetime.now().strftime('%d/%m/%Y') #Recording the time we begin the task
+        self.deadline = deadline
+
+    def __str__(self):
+        return f'{self.client_name} | {self.project} | {self.status}| {self.start_date} | {self.deadline}'
+    
+    def saving_projects(self, filename='projects.json'):
+        try: 
+            with open(filename, 'r') as file:    #To read and append to list if projects exist before.
+                projects_list = json.load(file)
+        except FileNotFoundError:
+            projects_list = []
+
+        projects = {
+        "client_name": self.client_name,
+        "project": self.project,
+        "status": self.status,
+        "start_date": self.start_date,
+        "deadline": self.deadline
+            }
+        
+        projects_list.append(projects)
+
+        try:
+            with open(filename, 'w') as project_file:
+                json.dump(projects_list, project_file, indent=4)
+        except FileNotFoundError:
+            print('Couldn\'t find the file.')
+
+        return projects_list
+    
+    
 '''This Functions are Under Manage Functions'''
 # Client Adding Section
 def add_client():
@@ -195,9 +235,71 @@ def manage_clients():
 '''End of Managing Client Section, Next section is project managing'''
 
 #Project Management section
-def add_projects():
-  
- pass 
+'''Client Verfication Fucntion for adding projects'''
+def client_verification(client_list): #This is helper function for the add projects function
+    if  not client_list:
+        print('Clients list is empty, Please add clients first!')
+        return None
+    
+    while True:
+        user_input = input('Enter client name or (type \"menu" to go back) ' ).strip()
+        
+        #Allowing to retunr to main menu
+        if user_input.lower() == "menu":
+            print('Returning to main menu...')
+            return None
+        
+        if not user_input or user_input.isdigit():
+            print('Invalid Input! Please enter the name of the client for checking!')
+            continue
+        #There is another function that returns the clients list
+
+        for client in client_list: 
+            if client['name'].lower() == user_input.lower():
+                print('Client exists, Lets proceed to add project!')
+                return client
+        print('Client Not found. Try agian')
+
+        #Add projcets function
+def add_projects(client_list):
+    choice = input('Add project to new client? Yes/No: ').strip().lower()
+    if choice == 'yes':
+        print('Please add client from main menu first.')
+        return 
+    elif choice == 'no':
+        client = client_verification(client_list)
+        if client:
+            project_type_input = input('What is the Project type? ').strip() 
+            #It can be anything(it can be represented with a number or anything)
+
+            allowed_statuses = ['Not Started', 'Started', 'Completed']
+            while True: 
+                status_input = input('What is the status of the project? choose form').strip()
+                if status_input not in allowed_statuses:
+                    print('Invalid Status! Please choose from', ", ".join(allowed_statuses))
+                    continue
+                else: 
+                    break
+
+            while True:
+                    project_deadline = input('When is the dedline?(dd/mm/yyyy) ')
+                    try:
+                        datetime.strptime(project_deadline, "%d/%m/%y")
+                        break
+                    except ValueError:
+                        print('Invalid date format. Please use dd/mm/yyyy')
+    else:
+        print('Invalid Choice. Please enter Yes/No.')
+ 
+
+    return client, project_type_input, status_input, project_deadline 
+
+
+# client, project_type_input, status_input, project_deadline = add_projects(client_list)
+# project = Projects(client, project_type_input, status_input, project_deadline)
+# project.saving_projects()
+# print('\n✅ Project Added Succesfully!\n')
+
 def view_projects():
     pass
 def update_project_status():    
@@ -214,20 +316,36 @@ def manage_projects():
                 4: delete_project,
                 5: None
     }
+    while True:
+        project_choices = [
+                        'Add Projects to Client',
+                        'View Client Projects',
+                        'Update Project Status',
+                        'Delete a Project',
+                        'Back to main Menu'
+                        ] 
+        for index, project_choice in enumerate(project_choices, start=1):
+            print(f'{index}: {project_choice}\n')
 
-    project_choices = [
-                    'Add Projects to Client',
-                    'View Client Projects',
-                    'Update Project Status',
-                    'Delete a Project',
-                    'Back to main Menu'
-                    ]
+        try:
+            user_manage_input =  int(input('What do you want to manage in projects? '))
+            action = project_function_mapping.get(user_manage_input)
+            if action:
+                action()
+            else:
+                print('Please choose the right number ')
+        except ValueError:
+            print('Please provide the right input! ')
+            continue
+        
+  
     
 # Main menu section 
 
 def main_menu():
     main_function_mapping = {
         1: manage_clients,
+        2: manage_projects,
         # Here function will be mapped. For every option in the main choice
     }
 
